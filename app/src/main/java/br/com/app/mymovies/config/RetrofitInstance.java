af4d6032c6f716d0c9d156eb97d007ab00bf8226
vitorakutsu <vitorakutsu@gmail.com>
2024-11-15 00:21:23 -0300

    feat: added retrofit instance

---
+++ app/src/main/java/br/com/app/mymovies/config/RetrofitInstance.java
@@ -0,0 +1,20 @@
+package br.com.app.mymovies.config;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitInstance {
+    private static Retrofit retrofit;
+    private static final String BASE_URL = "http://localhost:8080/";
+
+    public static Retrofit getRetrofitInstance() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
+
